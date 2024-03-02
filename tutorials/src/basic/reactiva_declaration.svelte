cc6aebbe96c0dad89c485efc75ba6718359aa4a8
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-02 10:50:27 +0530

    reactive declarations. in svelte

---
+++ tutorials/src/basic/reactiva_declaration.svelte
@@ -0,0 +1,14 @@
+<script>
+	let count = 0;
+	// reactive declarations.
+	$: doubled = count*2;
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+<p> {count} double is {doubled}</p>
