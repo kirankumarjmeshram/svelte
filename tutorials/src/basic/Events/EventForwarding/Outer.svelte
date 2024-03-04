df77f7fc00855a6c6347c15b2617ca2a5c72a99d
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-04 13:17:51 +0530

    Event Forwording

---
+++ tutorials/src/basic/Events/EventForwarding/Outer.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Inner from "./Inner.svelte";
+    // import { createEventDispatcher } from 'svelte';
+	// const dispatch = createEventDispatcher();
+	// function forward(event){
+	// 	dispatch('message',event.datail);
+	// }
+</script>
+<Inner on:message />
