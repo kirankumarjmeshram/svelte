9385e08bf6ce1559b10f6390e6f4c838bec0a9d1
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-04 13:21:52 +0530

    This means that not only will changes to the value of name update the input value, but changes to the input value will update name.

---
+++ tutorials/src/basic/Bindings/TextInputs.svelte
@@ -0,0 +1,9 @@
+<script>
+	let name = 'world';
+</script>
+
+<!-- <input value={name} /> -->
+<!-- This means that not only will changes to the value of name update the input value, but changes to the input value will update name. -->
+<input bind:value={name} />
+
+<h1>Hello {name}!</h1>
