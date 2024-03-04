1d60276e3ae1a9d016f33853470f7fa84780404c
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-04 11:42:10 +0530

    Component Event

---
+++ tutorials/src/basic/Events/Component Event/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Inner from './Inner.svelte';
+
+	function handleMessage(event) {
+		alert(event.detail.text);
+	}
+	
+</script>
+
+<Inner on:my_message = {handleMessage}/>
---
+++ tutorials/src/basic/Events/Component Event/Inner.svelte
@@ -0,0 +1,13 @@
+<script>
+	import {createEventDispatcher } from 'svelte'
+	function sayHello() {
+		dispatch('my_message', {
+			text: 'Hello!'
+		});
+	}
+	const dispatch = createEventDispatcher();
+</script>
+
+<button on:click={sayHello}>
+	Click to say hello
+</button>
