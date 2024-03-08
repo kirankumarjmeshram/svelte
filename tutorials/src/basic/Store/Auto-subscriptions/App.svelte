61ecb13eee561260621bb169c63ec6642bae368c
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-08 11:26:57 +0530

    stores auto-subscription

---
+++ tutorials/src/basic/Store/Auto-subscriptions/App.svelte
@@ -0,0 +1,23 @@
+<!-- Any name beginning with $ is assumed to refer to a store value. It's effectively a reserved character — Svelte will 
+    prevent you from declaring your own variables with a $ prefix. -->
+
+<script>
+    // import { onDestroy } from 'svelte';
+    import {count} from './stores';
+    import Increment from './Increment.svelte';
+    import Decrement from './Decrement.svelte';
+    import Res from './Resetter.svelte'
+
+	// let count_value;
+
+	// const unsubscribe = count.subscribe(value =>{
+	// 	count_value = value;
+	// })
+
+	// onDestroy(unsubscribe);
+</script>
+<!-- <h1>The count is {count_val}</h1> -->
+<h1>The count is {$count}</h1>
+<Increment/>
+<Decrement/>
+<Res/>
