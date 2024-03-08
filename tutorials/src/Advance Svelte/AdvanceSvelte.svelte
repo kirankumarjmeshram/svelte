d2ac7296838378e4361c42dcb8e8617c4427043b
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-08 12:44:13 +0530

    basic stracture created

---
+++ tutorials/src/Advance Svelte/AdvanceSvelte.svelte
@@ -0,0 +1,4 @@
+<script>
+    
+</script>
+<h1>ADVANCE SVELTE</h1>
---
+++ tutorials/src/basic/BAsicSvelte.svelte
@@ -0,0 +1,8 @@
+<script>
+    import ReadableStores from '../basic/Store/Readable stores/App.svelte'
+</script>
+<h1>BASIC SVELTE</h1>
+<h2>STORE</h2>
+<h3>Readable Store</h3>
+<ReadableStores/>
+
