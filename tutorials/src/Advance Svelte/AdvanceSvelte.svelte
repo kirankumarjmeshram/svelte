a8a290396a431ae5ae9ea61f8c3806ecd26b4710
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-19 12:35:02 +0530

    transaction directive in transation svelte

--- tutorials/src/Advance Svelte/AdvanceSvelte.svelte
+++ tutorials/src/Advance Svelte/AdvanceSvelte.svelte
@@ -1,9 +1,22 @@
 <script>
     import Spring from "./Motion/Spring.svelte";
 import Tweens from "./Motion/Tweens.svelte";
+  import CustomCssTransation from "./transitions/CustomCssTransation.svelte";
+  import CustomJsTransaction from "./transitions/CustomJSTransaction.svelte";
+  import InAndOut from "./transitions/InAndOut.svelte";
+  import TransitionDirective from "./transitions/TransitionDirective.svelte";
 </script>
 <h1><b>ADVANCE SVELTE</b></h1>
 <h2>Tweens</h2>
 <Tweens/>
 <h2>Spring</h2>
 <Spring/>
+<h2>The Transaction Directive</h2>
+<TransitionDirective/>
+<h2>In and Out</h2>
+<InAndOut/>
+<h2>Custom CSS Transation</h2>
+<CustomCssTransation />
+<h2>Custom JS Transaction</h2>
+<CustomJsTransaction/>
+<h2></h2>
