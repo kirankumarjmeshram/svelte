c0edcff1b3e3dafb267c9b04c48a47d9d944da1a
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-19 11:45:47 +0530

    motion spring

--- tutorials/src/Advance Svelte/AdvanceSvelte.svelte
+++ tutorials/src/Advance Svelte/AdvanceSvelte.svelte
@@ -1,6 +1,9 @@
 <script>
-    import Tweens from "./Motion/Tweens.svelte";
+    import Spring from "./Motion/Spring.svelte";
+import Tweens from "./Motion/Tweens.svelte";
 </script>
 <h1><b>ADVANCE SVELTE</b></h1>
 <h2>Tweens</h2>
 <Tweens/>
+<h2>Spring</h2>
+<Spring/>
