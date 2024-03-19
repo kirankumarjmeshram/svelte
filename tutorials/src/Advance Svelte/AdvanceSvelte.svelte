f7f440d78994a8d6efe542fd5ddae5de9dc6e46f
Kirankumar J Meshram <159528990+kirankumarjpinaca@users.noreply.github.com>
2024-03-19 15:13:08 +0530

    Differed transaction

--- tutorials/src/Advance Svelte/AdvanceSvelte.svelte
+++ tutorials/src/Advance Svelte/AdvanceSvelte.svelte
@@ -3,8 +3,12 @@
 import Tweens from "./Motion/Tweens.svelte";
   import CustomCssTransation from "./transitions/CustomCssTransation.svelte";
   import CustomJsTransaction from "./transitions/CustomJSTransaction.svelte";
+  import DifferedTransaction from "./transitions/Differed Transaction/DifferedTransaction.svelte";
+  import GlobalTransaction from "./transitions/GlobalTransaction.svelte";
   import InAndOut from "./transitions/InAndOut.svelte";
+  import KeyBlock from "./transitions/KeyBlock/KeyBlock.svelte";
   import TransitionDirective from "./transitions/TransitionDirective.svelte";
+  import TransitionEvents from "./transitions/TransitionEvents.svelte";
 </script>
 <h1><b>ADVANCE SVELTE</b></h1>
 <h2>Tweens</h2>
@@ -19,4 +23,11 @@ import Tweens from "./Motion/Tweens.svelte";
 <CustomCssTransation />
 <h2>Custom JS Transaction</h2>
 <CustomJsTransaction/>
-<h2></h2>
+<h2>Transition Events</h2>
+<TransitionEvents/>
+<h2>Global Transaction</h2>
+<GlobalTransaction/>
+<h2>Key Block</h2>
+<KeyBlock/>
+<h2>Differed Transaction</h2>
+<DifferedTransaction/>
