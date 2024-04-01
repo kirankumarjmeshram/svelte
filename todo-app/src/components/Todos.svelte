9e73209a924a584a5449a51d49a0d8760847d556
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-04-01 18:54:50 +0530

    Filtering to-dos by status

--- todo-app/src/components/Todos.svelte
+++ todo-app/src/components/Todos.svelte
@@ -24,6 +24,10 @@
       todos = [...todos,{id: newTodoId, name: newTodoName, completed: false}];
       newTodoName = '';
     }
+    let filter = 'all';
+    function filterTodos (filter, todos) {
+      return filter === 'active' ? todos.filter((t) => !t.completed):filter==='completed'?todos.filter((t)=>t.completed):todos;
+    }
 </script>
 
 <!-- Todos.svelte -->
@@ -44,17 +48,17 @@
   
     <!-- Filter -->
     <div class="filters btn-group stack-exception">
-      <button class="btn toggle-btn" aria-pressed="true">
+      <button class="btn toggle-btn" class:btn__primary={filter === 'all'} aria-pressed={filter === 'all'} on:click={() => filter = 'all'} >
         <span class="visually-hidden">Show</span>
         <span>All</span>
         <span class="visually-hidden">tasks</span>
       </button>
-      <button class="btn toggle-btn" aria-pressed="false">
+      <button class="btn toggle-btn" class:btn__primary={filter === 'active'} aria-pressed={filter === 'active'} on:click={() => filter = 'active'} >
         <span class="visually-hidden">Show</span>
         <span>Active</span>
         <span class="visually-hidden">tasks</span>
       </button>
-      <button class="btn toggle-btn" aria-pressed="false">
+      <button class="btn toggle-btn" class:btn__primary={filter === 'completed'} aria-pressed={filter === 'completed'} on:click={() => filter = 'completed'} >
         <span class="visually-hidden">Show</span>
         <span>Completed</span>
         <span class="visually-hidden">tasks</span>
@@ -77,7 +81,7 @@
    -->
     <!-- To-dos -->
     <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-      {#each todos as todo (todo.id)}
+      {#each filterTodos(filter, todos) as todo (todo.id)}
       <li class="todo">
         <div class="stack-small">
           <div class="c-cb">
