3ef203fc5b450101e8636e9718df4acfdd68d4db
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-04-02 12:47:25 +0530

    created filter button component

--- todo-app/src/components/Todos.svelte
+++ todo-app/src/components/Todos.svelte
@@ -1,5 +1,5 @@
 <script>
-  import App from "../App.svelte";
+  import FilterButton from "./FilterButton.svelte";
 
     export let todos = [];
     // We can tell Svelte that we want our totalTodos and completedTodos variables to be reactive by prefixing them with $:. 
@@ -47,24 +47,8 @@
     </form>
   
     <!-- Filter -->
-    <div class="filters btn-group stack-exception">
-      <button class="btn toggle-btn" class:btn__primary={filter === 'all'} aria-pressed={filter === 'all'} on:click={() => filter = 'all'} >
-        <span class="visually-hidden">Show</span>
-        <span>All</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-      <button class="btn toggle-btn" class:btn__primary={filter === 'active'} aria-pressed={filter === 'active'} on:click={() => filter = 'active'} >
-        <span class="visually-hidden">Show</span>
-        <span>Active</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-      <button class="btn toggle-btn" class:btn__primary={filter === 'completed'} aria-pressed={filter === 'completed'} on:click={() => filter = 'completed'} >
-        <span class="visually-hidden">Show</span>
-        <span>Completed</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-    </div>
-  
+    <!-- <FilterButton {filter} onclick = { (clicked) => filter = clicked}/> -->
+      <FilterButton bind:filter = {filter}/>
     <!-- TodosStatus -->
     <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
   
