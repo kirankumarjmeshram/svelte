859aad4308d6dd3191efaeb9aa8fa6f65a5b389f
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-04-16 18:36:16 +0530

    create case and uploading to db

---
+++ dashboard/src/components/CaseCreation/CreateCase.svelte
@@ -0,0 +1,30 @@
+<script>
+    import axios from 'axios';
+  
+    let title = ''; 
+  
+    async function postData() {
+      try {
+        const response = await axios.post('http://localhost:5123/post', { title });
+  
+        title = '';
+        
+        alert('Data posted successfully'); 
+      } catch (error) {
+        console.error(error);
+        alert('Failed to post data'); 
+      }
+    }
+  </script>
+  
+  <div>
+    <h2>Post Data</h2>
+    <form on:submit|preventDefault={postData}>
+      <label>
+        Title:
+        <input type="text" bind:value={title} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+  
