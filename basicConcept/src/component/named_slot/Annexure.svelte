9184d10925a1bfad5a7f4087c99f65e3c6867350
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-05-08 16:25:07 +0530

    worked on annexure table

---
+++ basicConcept/src/component/named_slot/Annexure.svelte
@@ -0,0 +1,49 @@
+<script>
+    import data from './data.json';
+  
+    const columns = [
+      'source_number',
+      'destination_number',
+      'call_type',
+      'date',
+      'time',
+      'first_cgid',
+      'Provider'
+    ];
+  </script>
+  
+<div>
+    <table>
+        <thead>
+          <tr>
+            {#each columns as column} <th>{column} </th> {/each}
+          </tr>
+        </thead>
+        <tbody>
+          {#each data as row}
+            <tr>
+              <td>{row.source_number}</td>
+              <td>{row.destination_number}</td>
+              <td>{row.call_type}</td>
+              <td>{row.date}</td>
+              <td>{row.time}</td>
+              <td>{row.first_cgid}</td>
+              <td>{row.Provider}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+</div>
+  
+  <style>
+    table, th, td {
+      border: 1px solid;
+      border-collapse: collapse;
+      padding: 5px;
+    }
+  </style>
+
+
+
+
+  
--- basicConcept/src/routes/+page.svelte
+++ basicConcept/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
     import App from "../component/named_slot/App.svelte";
+    import Annexure from "../component/named_slot/Annexure.svelte";
 </script>
 <App/>
+<Annexure/>
