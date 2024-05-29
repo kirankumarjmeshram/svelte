fa1c997d5fe5dcb5a6cd9ed494ba22800918f884
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-05-29 16:53:36 +0530

    updated signup  server page

---
+++ protected_routes/src/routes/signup/+page.server.js
@@ -0,0 +1,55 @@
+import { user } from "$lib/stores/user";
+import { redirect } from "@sveltejs/kit";
+
+export const actions = {
+    default: async ({cookies, request, fetch}) => {
+        const data = await request.formData()
+        const email = data.get('email')
+        const password = data.get('password')
+        const name = data.get('name')
+
+        if (typeof email !== 'string' || typeof password !== 'string' || !email || !password){
+            return {
+                status: 400,
+                body: {
+                    success: false,
+                }
+            }
+        }
+        const response = await fetch('http://127.0.0.1:3000/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user:{
+                    email,
+                    password,
+                    name
+                }
+            })
+        })
+
+    if(response.ok) {
+        const data = await response.json()
+        cookies.set('user', JSON.stringify(data.user))
+        cookies.set('jwt', response.headers.get('Authorization'))
+        let obj = {
+            ...data,
+            jwt: response.headers.get('Authorization')
+        }
+        user.set(obj)
+
+        throw redirect(302, '/')
+    }else{
+        const {error} = await response.json()
+        return {
+            status: 400,
+            body: {
+                success: false,
+                error
+                }
+            }
+        }
+    }
+}
--- protected_routes_server/app.py
+++ protected_routes_server/app.py
@@ -105,4 +105,4 @@ def signin():
     #     )
     return res
 if __name__ == '__main__':
-    app.run(debug = True, port = 3001)
+    app.run(debug = True, port = 3000)
